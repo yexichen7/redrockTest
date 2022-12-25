0f706411fdcd7281a44607da349885eca1e925fc
yexichen7 <2771872337@qq.com>
2022-12-25 14:20:10 +0800

    (a)

--- a.go
+++ a.go
@@ -1,18 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+)
 
+type user struct {
+	Username string    `json:"username"`
+	Nickname string    `json:"nickname"`
+	Sex      uint8     `json:"sex"`
+	Birthday time.Time `json:"birthday"`
+}
+
+// 结构体变量成员首字母未大写
 func main() {
-	var a = true
-	defer func() {
-		fmt.Println("1")
-	}()
-	defer func() {
-		fmt.Println("3")
-	}()
-	if a {
-		fmt.Println("2")
+	u := user{
+		Username: "坤坤",
+		Nickname: "阿坤",
+		Sex:      20,
+		Birthday: time.Now(),
+	}
+	bs, err := json.Marshal(&u)
+	if err != nil {
+		log.Println(err)
 		return
 	}
 
+	fmt.Println(string(bs))
 }
