f0f524c163c98743d3c84b1043b5fe7e266ec27f
yexichen7 <2771872337@qq.com>
2022-12-25 14:16:16 +0800

    Update a.go
    （a)

--- a.go
+++ a.go
@@ -1,32 +1,18 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"time"
-)
+import "fmt"
 
-type user struct {
-	Username string    `json:"username"`
-	Nickname string    `json:"nickname"`
-	Sex      uint8     `json:"sex"`
-	Birthday time.Time `json:"birthday"`
-}
-
-// 结构体变量成员首字母未大写
 func main() {
-	u := user{
-		Username: "坤坤",
-		Nickname: "阿坤",
-		Sex:      20,
-		Birthday: time.Now(),
-	}
-	bs, err := json.Marshal(&u)
-	if err != nil {
-		log.Println(err)
+	var a = true
+	defer func() {
+		fmt.Println("1")
+	}()
+	defer func() {
+		fmt.Println("3")
+	}()
+	if a {
+		fmt.Println("2")
 		return
 	}
 
-	fmt.Println(string(bs))
 }
